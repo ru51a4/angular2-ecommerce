282f6dab8859aa2f8cccbd3d011b4de3684fae21
ru51a4 <rruslanvaleev@yandex.ru>
2025-08-21 20:59:12 +0500

    modal fix

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { BuyModalComponent } from 'src/app/components/buy-modal/buy-modal.component';
+import { GlobalService } from 'src/app/global.service';
 
 @Component({
   selector: 'app-detail',
@@ -11,7 +13,7 @@ import { BuyModalComponent } from 'src/app/components/buy-modal/buy-modal.compon
 export class DetailComponent {
 
   public allFilter = false;
-  constructor(public dialog: MatDialog, private router: Router) {
+  constructor(public dialog: MatDialog, private router: Router, public service: GlobalService) {
 
   }
   public arr = [
@@ -30,13 +32,17 @@ export class DetailComponent {
     this.arr[id].curr = true;
   }
   buyModal() {
-    console.log('asd')
-    this.dialog.open(BuyModalComponent, {
+    let modal = this.dialog.open(BuyModalComponent, {
       data: {},
       hasBackdrop: true,
       backdropClass: '_',
       closeOnNavigation: true
     });
+    this.service.modals.pipe(filter((t) => !!t)).subscribe(() => {
+      modal.close()
+      this.service.modals.next(false);
+    })
+
   }
 
 }
