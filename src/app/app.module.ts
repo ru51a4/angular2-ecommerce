8285b7f131e1bd85cc63484a8586bbb0c4ed1773
ru51a4 <rruslanvaleev@yandex.ru>
2025-08-31 12:40:04 +0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from "@angular/material/dialog";
 import { StoreModule } from '@ngrx/store';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     AppRoutingModule,
     HttpClientModule,
     MatProgressBarModule,
+    FormsModule
   ],
   providers: [{ provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } }],
   bootstrap: [AppComponent]
